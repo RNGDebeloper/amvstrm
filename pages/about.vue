005ccc4f233fc093bd9922d957fa3c9399849366
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2023-12-31 19:35:39 +0530

    Update about.vue
    About Page Updated

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <script setup>
 useSeoMeta({
   ogTitle: "About",
-  ogDescription: "About amvstrm...",
+  ogDescription: "About Watch-Anime...",
   ogImage: "logo.png",
   ogUrl: "[og:url]",
   twitterTitle: "About",
-  twitterDescription: "About amvstrm...",
+  twitterDescription: "About Watch-Anime...",
   twitterImage: "logo.png",
   twitterCard: "summary",
 });
@@ -23,25 +23,18 @@ useHead({
       <div class="pa-5" style="background-color: #212121">
         <h1><img src="/logo.png" alt="loho" height="50" /></h1>
         <p>
-          amvstrm is an open source anime streaming platform that offer free
+          Watch Anime is an open source anime streaming platform that offer free
           anime with no ADs and install it as Android or IOS web app. <br />
-          Build with Nuxt 3, Vuetify and amvstrm's V2 API.
+          Build with Nuxt 3, Vuetify and Watch Anime's V2 API.
         </p>
         <div class="mt-2">
           <v-btn
             class="mr-2"
             color="grey-darken-1"
-            prepend-icon="mdi-github"
-            href="https://github.com/amvstrm"
+            prepend-icon="mdi-Telegram"
+            href="https://t.me/crunchyroll_co"
           >
-            Github
-          </v-btn>
-          <v-btn
-            color="blue-darken-1"
-            prepend-icon="mdi-file-document"
-            href="https://amvdocs.pages.dev"
-          >
-            Documentation
+            Telegram
           </v-btn>
         </div>
         <div class="my-5">
