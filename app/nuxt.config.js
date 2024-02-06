e5161cf20df24769aea2ac0178bca63b13d15bcc
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-02-06 22:08:54 +0530

    Create nuxt.config.js
    Add Google analytics

---
+++ app/nuxt.config.js
@@ -0,0 +1,15 @@
+// nuxt.config.js
+
+export default {
+  // Other Nuxt.js configurations...
+
+  modules: [
+    '@nuxtjs/google-analytics',
+  ],
+
+  googleAnalytics: {
+    id: 'G-0ER9KSVEH8', // Replace this with your Google Analytics ID
+  },
+
+  // Other module options...
+}
